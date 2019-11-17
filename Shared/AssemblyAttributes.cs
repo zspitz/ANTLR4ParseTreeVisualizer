6b569e1040bb4570f18292a2bde52ae280bf84c7
Zev Spitz <shivisi@hotmail.com>
2019-11-17 04:17:54 +0200

    Output single dll instead of 2019/2017

--- Shared/AssemblyAttributes.cs
+++ Shared/AssemblyAttributes.cs
@@ -9,16 +9,28 @@ using System.Threading.Tasks;
     visualizer: typeof(ParseTreeVisualizer.Visualizer),
     visualizerObjectSource: typeof(ParseTreeVisualizer.ObjectSource),
     Target = typeof(Antlr4.Runtime.RuleContext),
+#if ANTLR_RUNTIME
+    Description = "ANTLR4 Parse Tree Visualizer")]
+#else
     Description = "ANTLR4 Parse Tree Visualizer (Standard)")]
+#endif 
 
 [assembly: DebuggerVisualizer(
     visualizer: typeof(ParseTreeVisualizer.Visualizer),
     visualizerObjectSource: typeof(ParseTreeVisualizer.ObjectSource),
     Target = typeof(string),
+#if ANTLR_RUNTIME
+    Description = "ANTLR4 Parse Tree Visualizer")]
+#else
     Description = "ANTLR4 Parse Tree Visualizer (Standard)")]
+#endif 
 
 [assembly: DebuggerVisualizer(
     visualizer: typeof(ParseTreeVisualizer.Visualizer),
     visualizerObjectSource: typeof(ParseTreeVisualizer.ObjectSource),
     Target = typeof(Antlr4.Runtime.BufferedTokenStream),
+#if ANTLR_RUNTIME
+    Description = "ANTLR4 Parse Tree Visualizer")]
+#else
     Description = "ANTLR4 Parse Tree Visualizer (Standard)")]
+#endif 
