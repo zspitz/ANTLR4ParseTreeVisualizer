7f64ef368e1b14b6109bbb4e2c01dc68075a0605
Zev Spitz <shivisi@hotmail.com>
2021-07-11 16:27:35 +0300

    Debuggee

---
+++ Debuggee/VisualizerDataObjectSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using ParseTreeVisualizer.Serialization;
+using Periscope.Debuggee;
+
+namespace ParseTreeVisualizer {
+    public class VisualizerDataObjectSource : VisualizerObjectSourceBase<object, Config> {
+        public override object GetResponse(object target, Config config) => new VisualizerData(target, config);
+        public override string GetConfigKey(object target) => Assembly.GetAssembly(target.GetType()).GetName().Name;
+    }
+}
